be59a21416e03d9fd6f2af365f0bd17b1b400972
Александр Попов <aleksandrpopov_2003@mail.ru>
2023-04-26 19:25:18 +0300

    Добавил верстку

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const buttonAdd = document.querySelector('btn-add');
+const buttonCreate = document.querySelector('btn-create');
+const firstVert = document.querySelector('from-vert');
+const lastVert = document.querySelector('to-vert')
+const container = document.querySelector("container")
+
+buttonAdd.addEventListener("click", () => {
+   let vert = document.createElement("div")
+   vert.classList.add("vert")
+});
+
