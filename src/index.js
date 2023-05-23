54f8fe4637934a55beb6897d7e051cb899b405fa
Александр Попов <aleksandrpopov_2003@mail.ru>
2023-05-23 18:37:07 +0300

    Переделал всё под библиотеку cytoscape

--- src/index.js
+++ src/index.js
@@ -1,74 +1,96 @@
-// Получаем все объекты из html
-const buttonAdd = document.querySelector('#btn-add');
+import { Graph } from "./graph.js";
+
+const buttonAddVert = document.querySelector('#btn-add-vert');
+const buttonAddEdge = document.querySelector('#btn-add-edge');
 const buttonCreate = document.querySelector('#btn-create');
-const firstVert = document.querySelector('#from-vert');
-const lastVert = document.querySelector('#to-vert');
+const firstVertEdge = document.querySelector('#from-vert-edge');
+const lastVertEdge = document.querySelector('#to-vert-edge');
+const weightEdge = document.querySelector('#weightEdge');
+const firstVertPath = document.querySelector('#from-vert-path');
+const lastVertPath = document.querySelector('#to-vert-path');
 const field = document.querySelector("#field");
-const canvas = document.querySelector('#canvas');
 
-// массив вершин и счетчик для их номеров
-let vertList = [];
-let vert_count = 0;
+const graph = new Graph();
+let count = 0
 
-// Настройки canvas
-let ctx = canvas.getContext('2d');
+let cy = cytoscape({
+   container: document.getElementById('field'),
+   elements: [],
+   style: cytoscape.stylesheet()
+  .selector('edge')
+      .css({   
+         'width': 3,
+         'line-color': '#ccc',
+         'target-arrow-color': '#ccc',
+         'target-arrow-shape': 'triangle',
+         'curve-style': 'bezier',
+         'label': 'data(label)',
+         'font-size': '18px',
+         'color': 'red'
+      })
+    .selector('node')
+      .css({
+        'content': 'data(id)',
+        'text-valign': 'center',
+        'color': 'white',
+        'text-outline-width': 2,
+        'text-outline-color': '#888',
+        'background-color': '#888'
+      })
+    .selector(':selected')
+      .css({
+        'background-color': 'black',
+        'line-color': 'black',
+        'target-arrow-color': 'black',
+        'source-arrow-color': 'black',
+        'text-outline-color': 'black'
+      }),
+   })
 
-// Событие для создания вершины
-buttonAdd.addEventListener("click", () => {
-   let element = document.createElement("div");
-   element.innerHTML = vert_count++;
-   element.classList.add("vert");
-   field.prepend(element);
-   vertList.push(element);
 
-   // событие для переноса вершины 
-   element.addEventListener('mousedown', (event) => {
-      let box = field.getBoundingClientRect();
-      let limits = {
-         top: box.top + element.offsetHeight /2,
-         right: box.right - element.offsetWidth,
-         bottom: box.bottom - element.offsetHeight,
-         left: box.left + element.offsetWidth / 2
-       };
-      // (2) подготовить к перемещению:
-      // разместить поверх остального содержимого и в абсолютных координатах
-       element.style.zIndex = 1000;
-       // переместим в body, чтобы мяч был точно не внутри position:relative
-       document.body.append(element);
-       // и установим абсолютно спозиционированный мяч под курсор
+function initApp(){
+   // cy.add([
+   //    { group: 'nodes', data: { id: 'n1' }, position: { x: 100, y: 100 },},
+   //   { group: 'nodes', data: { id: 'n2' }, position: { x: 200, y: 200 }},
+   //   { group: 'edges', data: { id: 'e1-2', source: 'n1', target: 'n2', label: 0 } }
+   // ])
+   // count = 2
+   // graph.addVertex(1)
+   // graph.addVertex(2)
+   // graph.addEdge(1,2,0)
+}
+initApp()
 
-       moveAt(event.pageX, event.pageY);
-
-       // передвинуть мяч под координаты курсора
-      // и сдвинуть на половину ширины/высоты для центрирования
-      function moveAt(pageX, pageY) {
-         if(pageX > limits.right){
-            element.style.left = limits.right + 'px';
-         }
-         else if (pageX > limits.left){
-            element.style.left = pageX - element.offsetWidth / 2 + 'px';
-         }
-         if (pageY > limits.bottom){
-            element.style.top = limits.bottom + 'px';
-         }
-         else if (pageY > limits.top){
-            element.style.top = pageY - element.offsetHeight / 2 + 'px';
-         }
-      }
-
-      function onMouseMove(e) {
-         moveAt(e.pageX, e.pageY);
-      }
-
-      // (3) перемещать по экрану
-      document.addEventListener('mousemove', onMouseMove);
-
-      // (4) положить мяч, удалить более ненужные обработчики событий
-      element.onmouseup = function() {
-         document.removeEventListener('mousemove', onMouseMove);
-         element.onmouseup = null;
-      }
-   });
+buttonAddVert.addEventListener('click', () => {
+   cy.add([
+      { group: 'nodes', data: { id: 'n' + ++count }, position: { x: 100, y: 100 }}
+   ])
+   graph.addVertex(count)
 })
 
+buttonAddEdge.addEventListener('click', () => {
+   const from = Number(firstVertEdge.value)
+   const to = Number(lastVertEdge.value)
+   const weight = Number(weightEdge.value) 
+
+   if(graph.check(from, to)){
+      try {
+        cy.add({
+            group: 'edges', data: {id: `e${from}-${to}`, source: 'n'+from, target: 'n'+to, label: weight}
+         })
+         graph.addEdge(from, to, weight)
+      } catch (error) { 
+         alert('This edge already exist')
+      }
+   }
+})
+
+buttonCreate.addEventListener('click', () => {
+   const startVert = Number(firstVertPath.value)
+   const finishVert = Number(lastVertPath.value)
+
+   
+   console.log(graph.vertices)
+   console.log(graph.dijkstra(1))
+})
 
